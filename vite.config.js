90831f5f065752cb856c054f060323fa7110dc39
danielfanion <daniel.fanion@gmail.com>
2025-06-16 11:52:26 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,5 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/forkFrontDiapabete/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  base: '/forkFrontDiapabete/'
 })
